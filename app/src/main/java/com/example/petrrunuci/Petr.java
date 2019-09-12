74147f5c0bf6677473df5e9ef63407b7b13859f1
Ricardo Montes <montesr2.uci.edu>
2019-09-12 11:55:26 -0700

    Activity2 Test "Hello"

--- app/src/main/java/com/example/petrrunuci/Petr.java
+++ app/src/main/java/com/example/petrrunuci/Petr.java
@@ -4,54 +4,30 @@ import android.graphics.Bitmap;
 
 public class Petr extends Entity{
     private int petrHeight = Constants.SCREEN_HEIGHT;
-    private int lives;
     private int points;
     private int jumpHeight;
-    private boolean faceRight;
     private boolean jumping;
-    private Animation idleLeft;
     private Animation damage;
     private Animation jump;
-    private Animation jumpLeft;
 
 
     //Constructor
     public Petr(int posX, int posY){
         super(posX, posY, Constants.SCREEN_WIDTH/9, Constants.SCREEN_HEIGHT/3);
-        faceRight(true);
         setPoints(0);
-        setRelPos(posX + (Constants.SCREEN_WIDTH/20)/2);
         //setJumpHeight();
         setIdle(Constants.petrIdle, 2);
-        setIdleLeft(Constants.petrIdleLeft, 2);
         setMove1(Constants.petrWalk, 0.25);
-        setMove2(Constants.petrWalkLeft, 0.25);
         setDamage(Constants.petrHit, 2);
         setJump(Constants.petrJump, 2);
-        setJumpLeft(Constants.petrJumpLeft, 2);
 
         setCurrentAnimation(getIdle());
     }//Constructor
 
-    public void faceRight(boolean face){
-        faceRight = face;
-    }//faceRight
-
-    public void setLives(int lives){
-        this.lives = lives;
-    }//setLives
 
     private void setPoints(int points){
         this.points = points;
     }//setPoints
-
-    private void setIdleLeft(Bitmap[] sprites, double aniTime){
-        idleLeft = new Animation(sprites, aniTime);
-    }//setIdleLeft
-
-    private void setIdleLeft(Animation idleLeft){
-        this.idleLeft = idleLeft;
-    }//copy setIdleLeft
 
     private void setDamage(Bitmap[] sprites, double aniTime){
         damage = new Animation(sprites, aniTime);
@@ -69,12 +45,6 @@ public class Petr extends Entity{
         this.jump = jump;
     }//copy setJump
 
-    private void setJumpLeft(Bitmap[] sprites, double aniTime){
-        jumpLeft = new Animation(sprites, aniTime);
-    }//setJumpLeft
 
-    private void setJumpLeft(Animation jumpLeft){
-        this.jumpLeft = jumpLeft;
-    }//copy setJumpLeft
 
 }//petr class
