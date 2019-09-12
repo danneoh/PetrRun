244af773ba1a6eb2018533b33529f96110eb1756
Ricardo Montes <montesr2.uci.edu>
2019-09-12 12:38:57 -0700

    Activity2 Test "Hello"

--- app/src/main/java/com/example/petrrunuci/Engine.java
+++ app/src/main/java/com/example/petrrunuci/Engine.java
@@ -1,4 +1,22 @@
 package com.example.petrrunuci;
 
+import android.content.Context;
+
 public class Engine extends Thread {
-}
+    private World world;
+    private Activity2 activity;
+
+    private int gravity = Constants.SCREEN_HEIGHT/100;
+
+    /* Constructor */
+    public Engine(Context context, World world){
+        this.world = world;
+        activity = (Activity2) context;
+    }//Engine
+
+    /* Updating Methods */
+
+
+}//end Class Engine
+
+/* EOF */
